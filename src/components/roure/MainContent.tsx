ad3d498fd08e04a32ec159bfcac89d4e27c2b317
KiKaily <noelcresencio@gmail.com>
2025-10-19 17:01:24 +0200

    s

--- src/components/roure/MainContent.tsx
+++ src/components/roure/MainContent.tsx
@@ -24,12 +24,12 @@ const MainContent: React.FC<MainContentProps> = ({
     }
 
     const timers = [
-      setTimeout(() => setLoadingStage(1), 3500),
-      setTimeout(() => setLoadingStage(2), 1000),
-      setTimeout(() => setLoadingStage(3), 2000),
-      setTimeout(() => setLoadingStage(4), 3000),
-      setTimeout(() => setLoadingStage(5), 4000),
-      setTimeout(() => setLoadingStage(6), 4500),
+      setTimeout(() => setLoadingStage(1), 4000),
+      setTimeout(() => setLoadingStage(2), 2000),
+      setTimeout(() => setLoadingStage(3), 3000),
+      setTimeout(() => setLoadingStage(4), 4000),
+      setTimeout(() => setLoadingStage(5), 5000),
+      setTimeout(() => setLoadingStage(6), 6000),
     ];
 
     return () => timers.forEach(clearTimeout);
